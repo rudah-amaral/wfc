5eb438ba85a3d92b5f5683188cf9fb19e67b7c2e
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-08-01 17:50:45 -0300

    Feat: Add <App /> as first route to react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createHashRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./components/App";
 import "./global.scss";
 
+const router = createHashRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
