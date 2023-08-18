523af388e9703f67bf96110abc1dfa8a5d2d0634
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-08-18 15:38:55 -0300

    Refactor: Delete dead code

--- src/components/InputsWrapper/InputsWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import styles from "./InputsWrapper.module.scss";
-import { PropsWithChildren } from "react";
-
-export default function InputsWrapper(props: PropsWithChildren) {
-  return <div className={styles.inputs}>{props.children}</div>;
-}
