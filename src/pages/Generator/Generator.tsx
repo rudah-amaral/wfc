fe37286e0a6bc398acb9106bf8bd79d4028a4fd0
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-09-01 18:31:23 -0300

    Fix(Generator): Rectify loader's error message
    Since commit 8d81b1c it is possible to have grids with 1 columns and/or
    1 row.

--- src/pages/Generator/Generator.tsx
+++ src/pages/Generator/Generator.tsx
@@ -58,7 +58,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
   const urlParamsOutOfBounds =
     columns > 30 || columns < 1 || rows > 30 || rows < 1;
   if (urlParamsNotInt || urlParamsOutOfBounds) {
-    throw new Error("rows and columns should be integers between 2 and 30");
+    throw new Error("rows and columns should be integers between 1 and 30");
   }
 
   return { columns, rows };
