d0ff1ae7f7facfcfbc1dac528d94a821ba6cdedd
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-09-10 19:10:01 -0300

    Feat: Add a footer with contact info

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,43 @@
+import styles from "./Footer.module.scss";
+
+export default function Foorter() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <h4 className={styles.title}>You can find me on:</h4>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <span className={styles.contactLocation}>GitHub</span>
+            <a
+              href="https://www.github.com/rudah-amaral"
+              className={styles.link}
+            >
+              rudah-amaral
+            </a>
+          </li>
+          <li className={styles.listItem}>
+            <span className={styles.contactLocation}>LinkedIn</span>
+            <a
+              href="https://www.linkedin.com/in/rudahamaral/"
+              className={styles.link}
+            >
+              rudahamaral
+            </a>
+          </li>
+          <li className={styles.listItem}>
+            <span className={styles.contactLocation}>Twitter</span>
+            <a href="https://twitter.com/rudah_dev" className={styles.link}>
+              @rudah_dev
+            </a>
+          </li>
+          <li className={styles.listItem}>
+            <span className={styles.contactLocation}>Email</span>
+            <a href="mailto:rudah.dev@gmail.com" className={styles.link}>
+              rudah.dev@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer";
 import Header from "../Header";
+import styles from "./NavBar.module.scss";
 
 export default function NavBar() {
   return (
     <>
       <Header />
-      <main>
+      <main className={styles.main}>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
