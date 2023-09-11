0b71334bf9aadfa35eafec8bfed676297c22483a
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-09-11 19:21:04 -0300

    Feat: Add Home's content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,21 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.paragraphs}>
-      <p className={styles.paragraph}>
-        Sit incidunt corrupti quidem praesentium excepturi Sit explicabo eos
-        illo sunt nostrum. Facere esse aliquam odio corrupti numquam Ea neque
-        quis harum deserunt harum. Ut soluta corporis fuga sed illo, laboriosam
-        Quidem illum nam officia nihil aliquid Reiciendis excepturi nesciunt
-        dolore commodi tempore! Non ea quaerat laboriosam atque distinctio.
-        Dolor?
-      </p>
-      <p className={styles.paragraph}>
-        Elit non vitae commodi ratione ab. Laudantium expedita quo maiores non
-        expedita Quos blanditiis nam dolorem unde ipsam Mollitia id
-        reprehenderit sit sed harum? Deleniti ipsam molestiae quod rerum nulla
-        aperiam temporibus Rerum impedit et iure corporis eaque Reiciendis quas
-        fuga libero dolorem commodi. Accusamus neque deserunt inventore incidunt
-        fuga unde Explicabo reiciendis dolorem tempore odio blanditiis odit.
-        Rerum deserunt quod labore velit perferendis porro in molestiae dolorem?
-        Voluptatum suscipit in sit quia saepe Sit ipsum libero iste nostrum
-        neque Eum quisquam quas quo tempora delectus. Omnis esse earum facere
-        ipsa aperiam Expedita vel id rem harum iure Dicta dolorem.
-      </p>
-      <p className={styles.paragraph}>
-        Dolor doloribus officia quidem id suscipit? Quibusdam voluptatibus nulla
-        ipsa amet doloribus explicabo! Odit beatae ipsum perferendis voluptas
-        enim, animi?
-      </p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Visualize procedurally generated images</h1>
+      <h2 className={styles.subtitle}>
+        Unveil the potential of Wave Function Collapse&apos; simple tiled model!
+      </h2>
+      <div className={styles.buttons}>
+        <Link to="generator" className={styles.buttonLink}>
+          Try it out
+        </Link>
+        <Link to="about" className={styles.buttonLink}>
+          Learn about the project
+        </Link>
+      </div>
     </div>
   );
 }
