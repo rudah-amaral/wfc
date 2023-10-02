d45a83c4929ac666a413896e9d8bd323a2caf1a3
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-10-02 17:00:08 -0300

    Feat: Update to a more concise site URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wave-function-collapse/",
+  base: "/wfc/",
   plugins: [react()],
 });
