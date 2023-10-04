639163f4b2c396db966158e01ae338f067e80421
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-10-04 17:34:38 -0300

    Refactor(Tile): Drop glob import for Tile src
    Thinking back on it, I really was making things more complicated than
    necessary. Why glob import if I can just refer to the Tile img path
    directly?

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,15 +1,9 @@
 import styles from "./Tile.module.scss";
 
-const tilesPath = import.meta.glob<string>("../../circuit-tileset/*.png", {
-  import: "default",
-  eager: true,
-});
-
 interface TileProps {
   id: number;
 }
 
 export default function Tile({ id }: TileProps) {
-  const tilePath = tilesPath[`../../circuit-tileset/${id}.png`];
-  return <img src={tilePath} className={styles.tile} />;
+  return <img src={`/src/circuit-tileset/${id}.png`} className={styles.tile} />;
 }
