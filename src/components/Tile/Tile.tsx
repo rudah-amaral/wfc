02dc06427e86799d1d9c56788a62bcc89d5e14e3
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-11-20 20:07:26 -0300

    Revert "Refactor(Tile): Drop glob import for Tile src"
    This reverts commit 639163f4b2c396db966158e01ae338f067e80421.

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,9 +1,15 @@
 import styles from "./Tile.module.scss";
 
+const tilesPath = import.meta.glob<string>("../../circuit-tileset/*.png", {
+  import: "default",
+  eager: true,
+});
+
 interface TileProps {
   id: number;
 }
 
 export default function Tile({ id }: TileProps) {
-  return <img src={`/src/circuit-tileset/${id}.png`} className={styles.tile} />;
+  const tilePath = tilesPath[`../../circuit-tileset/${id}.png`];
+  return <img src={tilePath} className={styles.tile} />;
 }
