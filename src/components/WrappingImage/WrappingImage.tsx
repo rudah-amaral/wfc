53cce7c0020b80543e1e1a1a042b2179665a6eb3
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-12-03 19:14:05 -0300

    Feat(WrappingImage): Turn scrolling manual
    Some people have complained that having the final image to infinite
    scroll just as it is generated made them dizzy. In the effort of
    accessibility, I've deleted this feature and instead implemented one
    in which the user can drag the final image with his pointer, which also
    can be done indefinitely similar to the infinite scrolling.

--- src/components/WrappingImage/WrappingImage.tsx
+++ src/components/WrappingImage/WrappingImage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useLoaderData } from "react-router-dom";
 import styles from "./WrappingImage.module.scss";
 import type { ReturnOfGeneratorLoader } from "@/pages/Generator";
@@ -8,56 +8,24 @@ interface WrappingImageProps {
 }
 
 export default function WrappingImage({ gridTilesPaths }: WrappingImageProps) {
-  const [imgURL, setImageURL] = useState<string>();
-  const imgRef = useRef<HTMLImageElement>(null);
+  const mosaicDivRef = useRef<HTMLDivElement>(null);
   const { rows, columns } = useLoaderData() as ReturnOfGeneratorLoader;
 
   useEffect(() => {
-    let patternTileSize: number;
-    setScrollingImgURL().then(() => {
-      if (!imgRef.current) return;
+    getBackgroundImage().then((blobUrl) => {
+      if (!mosaicDivRef.current) return;
 
-      const animationDuration = patternTileSize * 50;
-      const xDuration = columns * animationDuration;
-      const yDuration = rows * animationDuration;
-      imgRef.current.style.animationDuration = `${xDuration}ms, ${yDuration}ms`;
+      mosaicDivRef.current.style.backgroundImage = `url(${blobUrl})`;
     });
 
-    async function setScrollingImgURL() {
-      const pattern = await createPattern();
-      if (!pattern || !patternTileSize) return;
-
-      const patternWidth = patternTileSize * columns;
-      const patternHeight = patternTileSize * rows;
-
-      const imageCanvas = document.createElement("canvas");
-      imageCanvas.width = patternWidth * 2;
-      imageCanvas.height = patternHeight * 2;
-
-      const ctx = imageCanvas.getContext("2d");
-      if (!ctx) return;
-
-      ctx.fillStyle = pattern;
-      let offsetY = 0;
-      for (let row = 0; row < 2; row++) {
-        let offsetX = 0;
-        for (let column = 0; column < 2; column++) {
-          ctx.fillRect(offsetX, offsetY, patternWidth, patternHeight);
-          offsetX += patternWidth;
-        }
-        offsetY += patternHeight;
-      }
-
-      setImageURL(imageCanvas.toDataURL());
-    }
-
-    async function createPattern() {
+    async function getBackgroundImage() {
       const tileImg = await loadTileImg(gridTilesPaths[0]);
-      patternTileSize = tileImg.width;
+      const tileWidth = tileImg.width;
+      const tileHeight = tileImg.height;
 
       const canvas = document.createElement("canvas");
-      canvas.width = patternTileSize * columns;
-      canvas.height = patternTileSize * rows;
+      canvas.width = tileWidth * columns;
+      canvas.height = tileHeight * rows;
 
       const ctx = canvas.getContext("2d");
       if (!ctx) return;
@@ -68,20 +36,18 @@ export default function WrappingImage({ gridTilesPaths }: WrappingImageProps) {
         for (let column = 0; column < columns; column++) {
           const tilePath = gridTilesPaths[column + row * columns];
           const tileImg = await loadTileImg(tilePath);
-          ctx.drawImage(
-            tileImg,
-            offsetX,
-            offsetY,
-            patternTileSize,
-            patternTileSize
-          );
-          offsetX += patternTileSize;
+          ctx.drawImage(tileImg, offsetX, offsetY, tileWidth, tileHeight);
+          offsetX += tileWidth;
         }
-        offsetY += patternTileSize;
+        offsetY += tileHeight;
       }
 
-      const pattern = ctx.createPattern(canvas, "repeat");
-      if (pattern) return pattern;
+      return new Promise<string>((resolve, reject) => {
+        canvas.toBlob((imgBlob) => {
+          if (imgBlob) resolve(URL.createObjectURL(imgBlob));
+          else reject("Couldn't convert canvas to blob");
+        });
+      });
     }
 
     function loadTileImg(tilePath: string) {
@@ -94,9 +60,53 @@ export default function WrappingImage({ gridTilesPaths }: WrappingImageProps) {
     }
   }, [columns, rows, gridTilesPaths]);
 
+  useEffect(() => {
+    if (!mosaicDivRef.current) return;
+    const mosaicDiv = mosaicDivRef.current;
+    mosaicDiv.addEventListener("pointerdown", startDrag);
+    document.addEventListener("pointerup", stopDrag);
+
+    return () => {
+      mosaicDiv.removeEventListener("pointerdown", startDrag);
+      document.removeEventListener("pointerup", stopDrag);
+    };
+
+    function startDrag() {
+      document.documentElement.style.userSelect = "none";
+      document.documentElement.style.cursor = "move";
+      document.addEventListener("pointermove", drag);
+    }
+
+    function stopDrag() {
+      document.documentElement.style.userSelect = "";
+      document.documentElement.style.cursor = "auto";
+      document.removeEventListener("pointermove", drag);
+    }
+
+    function drag(e: PointerEvent) {
+      if (!mosaicDivRef.current) return;
+      const mosaicDiv = mosaicDivRef.current;
+
+      const oldTop =
+        mosaicDiv.style.top === "" ? 0 : parseInt(mosaicDiv.style.top);
+      const oldLeft =
+        mosaicDiv.style.left === "" ? 0 : parseInt(mosaicDiv.style.left);
+
+      const newTop =
+        (oldTop - mosaicDiv.clientHeight + e.movementY) %
+        (mosaicDiv.clientHeight / 2);
+      const newLeft =
+        (oldLeft - mosaicDiv.clientWidth + e.movementX) %
+        (mosaicDiv.clientWidth / 2);
+
+      mosaicDiv.style.top = `${newTop}px`;
+      mosaicDiv.style.left = `${newLeft}px`;
+    }
+  });
+
   return (
     <div className={styles.scrollingImageWrapper}>
-      <img src={imgURL} ref={imgRef} className={styles.scrollingImage} />
+      <div ref={mosaicDivRef} className={styles.scrollingImage}></div>
     </div>
   );
 }
