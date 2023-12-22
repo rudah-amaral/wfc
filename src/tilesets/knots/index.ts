54f770a2bdd938692f87fd743d9cb2ad1b06a77a
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-12-22 17:44:18 -0300

    Feat: Add knots tileset
    One step closer to adding multiple tileset support. Now the only thing
    to implement is a component with the capacity to change the current
    tileset URLParam.

---
+++ src/tilesets/knots/index.ts
@@ -0,0 +1,1 @@
+export { baseTileset } from "./knots";
---
+++ src/tilesets/knots/knots.ts
@@ -0,0 +1,40 @@
+import type { BaseTile } from "@/tilesets";
+
+const tilesMod = import.meta.glob<string>("./*.png", {
+  import: "default",
+  eager: true,
+});
+
+const baseTileset: BaseTile[] = [
+  {
+    name: "corner",
+    path: tilesMod["./corner.png"],
+    edges: ["A", "A", "B", "B"],
+    timesCanBeRotated: 3,
+  },
+  {
+    name: "cross",
+    path: tilesMod["./cross.png"],
+    edges: ["A", "A", "A", "A"],
+    timesCanBeRotated: 1,
+  },
+  {
+    name: "empty",
+    path: tilesMod["./empty.png"],
+    edges: ["B", "B", "B", "B"],
+  },
+  {
+    name: "line",
+    path: tilesMod["./line.png"],
+    edges: ["B", "A", "B", "A"],
+    timesCanBeRotated: 1,
+  },
+  {
+    name: "t",
+    path: tilesMod["./t.png"],
+    edges: ["B", "A", "A", "A"],
+    timesCanBeRotated: 3,
+  },
+];
+
+export { baseTileset };
